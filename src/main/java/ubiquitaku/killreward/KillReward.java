4f4f7f38da11defc0d16c77fa8ef3d900a5b31b5
ubiquitaku <ubiquitaku@outlook.jp>
2021-06-10 21:29:50 +0900

    不具合修正、要素追加

--- src/main/java/ubiquitaku/killreward/KillReward.java
+++ src/main/java/ubiquitaku/killreward/KillReward.java
@@ -13,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 
 public final class KillReward extends JavaPlugin implements @NotNull Listener {
     FileConfiguration config;
+    boolean objectDeath;
+    String prefix = "§l[§r§a§lKillReward§r§l]§r";
     int rem;
     int get;
     VaultManager vault;
@@ -23,6 +25,7 @@ public final class KillReward extends JavaPlugin implements @NotNull Listener {
         vault = new VaultManager();
         saveDefaultConfig();
         config = getConfig();
+        objectDeath = config.getBoolean("obj");
         rem = config.getInt("removeMoney");
         get = config.getInt("getMoney");
         Bukkit.getPluginManager().registerEvents(this,this);
@@ -41,38 +44,77 @@ public final class KillReward extends JavaPlugin implements @NotNull Listener {
                 return true;
             }
             if (args.length == 0) {
-                sender.sendMessage("説明面倒だからこれで察して");
-                sender.sendMessage("/killrew rem <消す割合(%)>");
-                sender.sendMessage("/killrew get <殺人鬼が得る割合(%)>");
-                sender.sendMessage("消す割合より得る割合がでかいと増えます");
-                sender.sendMessage("どちらの割合も単位は%で保存されます(10ってやったら10%)");
+                sender.sendMessage("--------------------"+prefix+"--------------------");
+                sender.sendMessage("§d説明面倒だからこれで察して");
+                sender.sendMessage("§d/killrew reload : 設定ファイル(config.yml)のリロードを行います");
+                sender.sendMessage("§d/killrew rem <殺された人から消える金額の割合(%)>");
+                sender.sendMessage("§d/killrew get <殺した人が殺された人の所持金から得る割合(%)>");
+                sender.sendMessage("§d/killrew obj <on or off> : PK以外で死んだときに所持金の削除を行うか");
+                sender.sendMessage("§d↑※行う場合は削除のみ、変更は再起動等を行っても維持されます");
+                sender.sendMessage("§d消す割合より得る割合がでかいと増殖などが可能となります");
+                sender.sendMessage("§dどちらの割合も単位は%で保存されます(10ってやったら10%)");
+                sender.sendMessage("--------------------------------------------------");
                 return true;
             }
             if (args[0].equals("rem")) {
                 if (args.length != 2) {
-                    sender.sendMessage("argsの数が間違っています");
+                    sender.sendMessage(prefix+"argsの数が間違っています");
                     return true;
                 }
                 config.set("removeMoney",args[1]);
                 rem = Integer.parseInt((args[1]));
                 configReload();
-                sender.sendMessage("消す割合を設定しました");
+                sender.sendMessage(prefix+"殺されたプレイヤーから削除される割合を変更しました");
                 return true;
             }
             if (args[0].equals("get")) {
                 if (args.length != 2) {
-                    sender.sendMessage("argsの数が間違っています");
+                    sender.sendMessage(prefix+"argsの数が間違っています");
                     return true;
                 }
                 config.set("getMoney",args[1]);
                 get = Integer.parseInt((args[1]));
                 configReload();
-                sender.sendMessage("得る割合を設定しました");
+                sender.sendMessage(prefix+"殺したプレイヤーが得る割合を変更しました");
+                return true;
             }
-//            if ()
-//            if (!(sender instanceof Player)) return true;
-//            Player player = (Player) sender;
-//            vault.deposit(player, 100);
+            if (args[0].equals("reload")) {
+                reloadConfig();
+                config = getConfig();
+                objectDeath = config.getBoolean("obj");
+                rem = config.getInt("removeMoney");
+                get = config.getInt("getMoney");
+                sender.sendMessage(prefix+"リロード完了");
+                return true;
+            }
+            if (args[0].equals("obj")) {
+                if (args.length != 2) {
+                    sender.sendMessage(prefix+"argsの数が間違っています");
+                    return true;
+                }
+                if (args[1].equals("on")) {
+                    if (objectDeath) {
+                        sender.sendMessage(prefix+"既にonになっています");
+                        return true;
+                    }
+                    objectDeath = true;
+                    config.set("obj",true);
+                    configReload();
+                    sender.sendMessage(prefix+"PK以外の死亡でも所持金が減るようになりました");
+                    return true;
+                }
+                if (args[1].equals("off")) {
+                    if (!objectDeath) {
+                        sender.sendMessage(prefix+"既にoffになっています");
+                        return true;
+                    }
+                    objectDeath = false;
+                    config.set("obj",false);
+                    configReload();
+                    sender.sendMessage(prefix+"PK以外の死亡による所持金削除が無効化されました");
+                    return true;
+                }
+            }
         }
         return true;
     }
@@ -86,14 +128,20 @@ public final class KillReward extends JavaPlugin implements @NotNull Listener {
             return;
         }
         Player dead;
+        dead = e.getEntity();
         Player killer;
+        double value = vault.getBalance(dead);
+        if (!Bukkit.getOnlinePlayers().contains(e.getEntity().getKiller())) {
+            if (objectDeath) {
+                vault.withdraw(dead,value*rem/100);
+            }
+            return;
+        }
         try {
-            dead = e.getEntity();
             killer = e.getEntity().getKiller();
         } catch (NullPointerException exception) {
             return;
         }
-        double value = vault.getBalance(dead);
         vault.withdraw(dead,Math.round(value*rem/100));
         vault.deposit(killer,Math.round(value*get/100));
     }
@@ -101,6 +149,5 @@ public final class KillReward extends JavaPlugin implements @NotNull Listener {
     public void configReload() {
         saveConfig();
         config = getConfig();
-
     }
 }
